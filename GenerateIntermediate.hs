1cbecd6576e9eea7dd4b46a2096753082bb0bbb7
Caitlin Mangan <caitiem42@gmail.com>
2014-09-23 10:21:53 +1000

    start tidying up GenerateIntermediate: pull State stuff out into separate file

--- GenerateIntermediate.hs
+++ GenerateIntermediate.hs
@@ -1,60 +1,23 @@
 module GenerateIntermediate where
 
+import State
 import AbstractSyntaxTree
 import Data.Either
 import Data.List -- unfoldr
 import Data.Char (isSpace) -- for stripping leading whitespace
 
 
-
----- keep track of the number of blocks and registers used in the current method ----
-
-data UsedBlocks = Blocks Integer
-  deriving Show
-
-data UsedRegisters = Registers Integer
-  deriving Show
-
-data State = Used UsedBlocks UsedRegisters
-  deriving Show
-
-updateBlocks :: State -> State
-updateBlocks (Used (Blocks b) r') = (Used (Blocks (b + 1)) r')
-
-updateRegisters :: State -> State
-updateRegisters (Used b' (Registers r)) = (Used b' (Registers (r + 1)))
-
--- next unassigned register
-nextRegister :: State -> Integer
-nextRegister (Used b' (Registers r)) = r + 1
-
--- most recently assigned register
-prevRegister :: State -> Integer
-prevRegister (Used b' (Registers r)) = r
-
--- next unassigned block
-nextBlock :: State -> Integer
-nextBlock (Used (Blocks b) r') = b + 1
-
--- I don't know why I wrote a bind function. It seemed like a good idea at the time
-bind :: (State, String) -> (State -> (State, String)) -> (State, String)
-bind (state, str) f = let (state', str') = f state in (state', (str ++ str'))
-
-
-
 ------- Convert AST to String representing Intermediate Code ---------
 
-
+-- TODO:
+-- left = "( "; loadIntoNextReg id; etc
+-- tidy up generateIntermediate, arguments, statements, psStatements
 
 generateIntermediate :: Program -> String
-generateIntermediate ast = "( " ++ ( functions ast ) ++ ")"
-
-functions :: Program -> String
-functions (Program fs) 
-   = foldr (\(Function (ID id) a v s) 
-                -> (++) ("( " ++ id ++ " (" ++ (arguments a) ++ ") " ++ (statements s) ++ " ) " ))
-           "" fs 
-
+generateIntermediate (Program fs) 
+   = "( " ++ (foldr (\(Function (ID id) a v s) 
+                -> (++) ("( " ++ id ++ " (" ++ (arguments a) ++ ") " ++ (statements s) ++ " )" ))
+           "" fs) ++ " )"
 
 arguments :: Arguments -> String
 arguments (Arguments as) = dropWhile isSpace (foldr (\(ID a) -> (++) (" " ++ a)) "" as)
---
+++ State.hs
@@ -0,0 +1,36 @@
+module State where
+
+
+---- keep track of the number of blocks and registers used in the current method ----
+
+data UsedBlocks = Blocks Integer
+  deriving Show
+
+data UsedRegisters = Registers Integer
+  deriving Show
+
+data State = Used UsedBlocks UsedRegisters
+  deriving Show
+
+updateBlocks :: State -> State
+updateBlocks (Used (Blocks b) r') = (Used (Blocks (b + 1)) r')
+
+updateRegisters :: State -> State
+updateRegisters (Used b' (Registers r)) = (Used b' (Registers (r + 1)))
+
+-- next unassigned register
+nextRegister :: State -> Integer
+nextRegister (Used b' (Registers r)) = r + 1
+
+-- most recently assigned register
+prevRegister :: State -> Integer
+prevRegister (Used b' (Registers r)) = r
+
+-- next unassigned block
+nextBlock :: State -> Integer
+nextBlock (Used (Blocks b) r') = b + 1
+
+-- I don't know why I wrote a bind function. It seemed like a good idea at the time
+bind :: (State, String) -> (State -> (State, String)) -> (State, String)
+bind (state, str) f = let (state', str') = f state in (state', (str ++ str'))
+
